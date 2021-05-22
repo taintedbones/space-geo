75e26f37d402449fce2fb17989b3d1d93876d0a4
taintedbones <kjust@csu.fullerton.edu>
2021-05-21 19:36:03 -0700

    Added constant app footer

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    width: "inherit",
+    height: "50px",
+    position: "relative",
+    bottom: 0,
+    paddingTop: "10px",
+    marginTop: "15px",
+    backgroundColor: "gainsboro",
+  },
+}));
+
+function AppFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.body}>
+      <Typography variant="button">
+        <center>Webpage designed and create by: Kierstyn Just</center>
+      </Typography>
+    </div>
+  );
+}
+
+export default AppFooter;
