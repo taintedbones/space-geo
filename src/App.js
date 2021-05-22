873a997eed97182042ac71685d6fc3c82d812985
taintedbones <kjust@csu.fullerton.edu>
2021-05-21 19:35:32 -0700

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import { Container, makeStyles } from '@material-ui/core';
-import { 
-  BrowserRouter as Router,
-  Route,
-  Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ExpToolsPage from "./pages/ExpToolsPage";
 import GeoFeaturesPage from "./pages/GeoFeaturesPage";
 import GeoMoonPage from "./pages/GeoMoonPage";
 import GeoPlanetsPage from "./pages/GeoPlanetsPage";
 import SourcesPage from "./pages/SourcesPage";
-import TopNav from './components/TopNav';
-import Background from './assets/Space-Background.jpg';
+import TopNav from "./components/TopNav";
+import Background from "./assets/Space-Background.jpg";
+import AppFooter from "./components/Footer.js";
+import '@fontsource/roboto';
 
 const b_margins = "150px";
 
 const useStyles = makeStyles((theme) => ({
   app: {
     backgroundImage: `url(${Background})`,
-    backgroundRepeat: 'repeat-y',
-    backgroundSize: '100%',
-    backgroundAttachment: 'fixed',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
+    backgroundRepeat: "repeat-y",
+    backgroundSize: "100%",
+    backgroundAttachment: "fixed",
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
   body: {
-    background: 'white',
-    position: 'relative',
-    backgroundAttachment: 'fixed',
+    background: "white",
+    position: "relative",
     marginLeft: b_margins,
     marginRight: b_margins,
-    paddingTop: '100px',
-    height: '100%',
-    width: '1000px',
-    minWidth: '1000px'
-  }
+    height: "100%",
+    width: "1000px",
+    minWidth: "1000px",
+  },
 }));
 
 function App() {
@@ -44,8 +42,8 @@ function App() {
     section: "h4",
     sub_sec: "h5",
     body: "body1",
-    caption: "caption"
-  }
+    caption: "caption",
+  };
 
   return (
     <div className={classes.app}>
@@ -53,21 +51,26 @@ function App() {
         <Router>
           <Route path="/" component={TopNav} />
           <Switch>
-              <Route path="/space-geo/exp-tools">
-                <ExpToolsPage v={format} />
-              </Route>
-              <Route path="/space-geo/features" component={GeoFeaturesPage} />
-              <Route path="/space-geo/moon">
-                <GeoMoonPage v={format} />
-              </Route>
-              <Route path="/space-geo/planets">
-                <GeoPlanetsPage v={format} />
-              </Route>
-              <Route path="/space-geo/sources" component={SourcesPage} />
-              <Route path="/space-geo">
-                <HomePage v={format} />
-              </Route>
+            <Route path="/space-geo/exp-tools">
+              <ExpToolsPage v={format} />
+            </Route>
+            <Route path="/space-geo/features">
+              <GeoFeaturesPage v={format} />
+            </Route>
+            <Route path="/space-geo/moon">
+              <GeoMoonPage v={format} />
+            </Route>
+            <Route path="/space-geo/planets">
+              <GeoPlanetsPage v={format} />
+            </Route>
+            <Route path="/space-geo/sources">
+              <SourcesPage v={format} />
+            </Route>
+            <Route path="/space-geo">
+              <HomePage v={format} />
+            </Route>            
           </Switch>
+          <Route path="/" component={AppFooter} />          
         </Router>
       </div>
     </div>
